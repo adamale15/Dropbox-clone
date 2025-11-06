addc35662b12ad95c81c7ac8e5a5cf6fc4c26fb1
adamale15 <adamale@asu.edu>
2025-11-05 21:30:18 -0700

    Added the file operations like star, delete, trash, empty-trash

---
+++ app/api/files/upload/route.ts
@@ -0,0 +1,122 @@
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { and, eq } from "drizzle-orm";
+import ImageKit from "imagekit";
+import { v4 as uuidv4 } from "uuid";
+import { NextRequest, NextResponse } from "next/server";
+
+// Imagekit credentials
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
+  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || "",
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    //Parse formdata
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+    const formUserId = formData.get("userId") as string;
+    const parentId = (formData.get("parentId") as string) || null;
+
+    //Match it with userId
+    if (formUserId !== userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // If file exists
+    if (!file) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // Ensure parentId exists
+    if (parentId) {
+      //It belongs to user
+      const [parentFolder] = await db
+        .select()
+        .from(files)
+        .where(
+          and(
+            eq(files.id, parentId), //Ensure it exists
+            eq(files.userId, userId), // Ensure it belongs to the user
+            eq(files.isFolder, true) // Ensure that it is a folder
+          )
+        );
+    }
+    if (!parentId) {
+      return NextResponse.json(
+        { error: "Parent folder not found" },
+        { status: 401 }
+      );
+    }
+
+    // Check if file is of type Image/pdf
+    if (!file.type.startsWith("image/") && file.type !== "application/pdf") {
+      return NextResponse.json(
+        { error: "Only images and pdf are supported" },
+        { status: 401 }
+      );
+    }
+
+    //Convert file into buffer
+    const buffer = await file.arrayBuffer();
+    const fileBuffer = Buffer.from(buffer);
+
+    //Generate filename
+    const folderPath = parentId
+      ? `/droply/${userId}/folder/${parentId}`
+      : `/droply/${userId}`;
+
+    const originalFileName = file.name;
+
+    const fileExtension = originalFileName.split(".").pop() || "";
+
+    //Check for empty extension
+    if (!fileExtension) {
+      return NextResponse.json(
+        { error: "File must have an extension" },
+        { status: 400 }
+      );
+    }
+    // Validation for not storing exe, php, etc
+    const uniqueFileName = `${uuidv4}.${fileExtension}`;
+
+    //Upload file to imageKit
+    const uploadResponse = await imagekit.upload({
+      file: fileBuffer,
+      fileName: uniqueFileName,
+      folder: folderPath,
+      useUniqueFileName: false,
+    });
+
+    const fileData = {
+      name: originalFileName,
+      path: uploadResponse.filePath,
+      size: file.size,
+      type: file.type,
+      fileUrl: uploadResponse.url,
+      thumbnailUrl: uploadResponse.thumbnailUrl || null,
+      userId: userId,
+      parentId: parentId,
+      isFolder: false,
+      isStarred: false,
+      isTrash: false,
+    };
+
+    const [newFile] = await db.insert(files).values(fileData).returning();
+
+    return NextResponse.json(newFile);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to upload file" },
+      { status: 401 }
+    );
+  }
+}
